98bad661d938cc858963e733cf0d9dfda9f7ff24
amsyarkissmi <126231338+amsyarkissmi@users.noreply.github.com>
2023-09-14 15:14:25 +0800

    Delete penperel directory

--- penperel/admin/padam.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '../conn.php';
-
-$idwarden = $_GET['idwarden'];
-$sql = "DELETE FROM warden where idwarden = $idwarden";
-$conn->query($sql);
-
-header('location:index.php?menu=warden');
--- penperel/index.php
+++
@@ -1,9 +0,0 @@
-<h1 class="login-heading">Sistem Pengurusan Peralatan Elektrik Pelajar</h1>
-<h3 class="login-subheading">Log Masuk</h3>
-<form action="login.php" method="post" class="login-form">
-    <label for="idpengguna">ID Pengguna</label>
-    <input type="text" name="idpengguna" id="idpengguna" required><br>
-    <label for="kata">Kata Laluan</label>
-    <input type="password" name="kata" id="kata" required><br>
-    <button type="submit">MASUK</button>
-</form>
