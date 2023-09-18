5263c2f4d0356ae018af80a3421a18838fb44d36
amsyarkissmi <126231338+amsyarkissmi@users.noreply.github.com>
2023-09-18 16:53:33 +0800

    Delete untitled6 directory

--- untitled6/passcincang.php
+++
@@ -1,3 +0,0 @@
-<?php
-$kata=password_hash('zubir',PASSWORD_BCRYPT);
-echo $kata;
--- untitled6/warden/padam.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '../conn.php';
-
-$idpelajar = $_GET['idpelajar'];
-$sql = "DELETE FROM pelajar where idpelajar = $idpelajar";
-$conn->query($sql);
-
-header('location:index.php?menu=pelajar');
